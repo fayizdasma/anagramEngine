825bcf3670d5f3661322403f333b983e2f67486f
Fayis Musthafa <fayis.musthafa@student.griffith.ie>
2019-04-01 19:55:52 +0100

    bracket 4 done search restricted only alpha letters as input

--- anagram_engine/main.py
+++ anagram_engine/main.py
@@ -5,6 +5,7 @@ from google.appengine.ext import ndb
 import os
 
 from userModel import UserModel
+from wordModel import WordModel
 from addWord import AddWord
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -17,6 +18,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
+        isSearchClicked = False
+        search_result = None
         response_url = ''
         response_url_string = ''
         user = users.get_current_user()
@@ -38,14 +41,34 @@ class MainPage(webapp2.RequestHandler):
             response_url = users.create_login_url(self.request.uri)
             response_url_string = 'login'
 
+        # when search button is clicked
+        if self.request.get('button') == 'Search':
+            isSearchClicked = True
+            searchWord = self.request.get('word_name')
+            sortedWord = generate_key(searchWord)
+            anagram = ndb.Key(WordModel, sortedWord).get()
+            if anagram != None:
+                search_result = anagram.wordList
+
         # pass values to the html page
         template_values = {
             'url': response_url,
             'url_string': response_url_string,
             'user': user,
+            'search_result': search_result,
+            'isSearchClicked': isSearchClicked
         }
         main_template = JINJA_ENVIRONMENT.get_template('/templates/homePage.html')
         self.response.write(main_template.render(template_values))
+
+
+# sort all letters of the word into lexicographical order
+def generate_key(param):
+    sorted_list = sorted(param)
+    sorted_string = ''
+    for x in sorted_list:
+        sorted_string = sorted_string + x
+    return sorted_string
 
 
 app = webapp2.WSGIApplication([('/', MainPage), ('/add', AddWord)], debug=True)
